014d2bdf866a49181ebf8de11258b2f88cb82c1d
David Freitag <freitagdavid@outlook.com>
2024-02-22 16:12:59 -0500

    Added recipes really this time

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,26 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
 	type: 'content',
+
 	// Type-check frontmatter using a schema
 	schema: z.object({
+		layout: z.string(),
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		author: z.string(),
 	}),
-});
+})
 
-export const collections = { blog };
+const recipes = defineCollection({
+	type: 'content',
+	schema: z.object({
+		layout: z.string(),
+		title: z.string(),
+		image: z.string(),
+		imagecredit: z.string(),
+		// tags: z.string(),
+		// ingredients: z.array(z.string()),
+		// directions: z.array(z.string())
+	})
+})
+
+export const collections = { blog, recipes };
