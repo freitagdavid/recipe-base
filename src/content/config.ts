ea199cf834b2fa2a85b6a5fa5500252de14e2776
David Freitag <freitagdavid@outlook.com>
2024-02-29 18:20:55 -0500

    change collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { defineCollection, z } from 'astro:content';
-import { Recipe } from 'src/schemas';
 
 const blog = defineCollection({
 	type: 'content',
@@ -12,34 +11,16 @@ const blog = defineCollection({
 	}),
 })
 
-const nutritionInformation = z.object({
-	calories: z.number(),
-	carbohydrateContent: z.number(),
-	cholesterolContent: z.number(),
-
-})
-
-// const recipes = defineCollection({
-// 	type: 'content',
-// 	schema: ({ image, }) => z.object({
-// 		title: z.string(),
-// 		image: image(),
-// 		imagecredit: z.string().optional(),
-// 		tags: z.string().transform((val) => val.split(',')).optional(),
-// 		ingredients: z.array(z.string()).optional(),
-// 		components: z.array(z.string()).optional(),
-// 		directions: z.array(z.string()),
-// 		// In seconds
-// 		cookTime: z.number().optional(),
-// 		cookingMethod: z.string().optional(),
-// 		nutrition: 
-// 	})
-// })
-
 const recipes = defineCollection({
 	type: 'content',
-	schema: ({ image }) => Recipe.extend({
+	schema: ({ image, }) => z.object({
+		title: z.string(),
 		image: image(),
+		imagecredit: z.string().optional(),
+		tags: z.string().transform((val) => val.split(',')).optional(),
+		ingredients: z.array(z.string()).optional(),
+		components: z.array(z.string()).optional(),
+		directions: z.array(z.string())
 	})
 })
 
